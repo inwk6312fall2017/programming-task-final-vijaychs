b3c66218c864dfefed5bba06306e0be8fdf5fac9
inwkstudent <kalsiashish@yahoo.com>
2017-10-20 15:41:06 -0300

    created a dictionary of all the access lists! completed task 3

--- task03-2.py
+++ task03-2.py
@@ -3,5 +3,5 @@ with open("running-config.cfg") as document:
 	for line in document:
 		stripped_line = line.strip()
 		if stripped_line.startswith("access-list"):
-			access_list[stripped_line[0:10]] = stripped_line[10:]
-		print(access_list)
+			access_list[stripped_line[0:11]] = stripped_line[11:]
+			print(access_list)

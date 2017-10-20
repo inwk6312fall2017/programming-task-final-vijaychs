90bf7fab36dbef89764c8bbed169a1e84af3d4e1
inwkstudent <kalsiashish@yahoo.com>
2017-10-20 16:18:52 -0300

    adding name and mac address

--- get-network-hosts.py
+++ get-network-hosts.py
@@ -66,5 +66,7 @@ print (json.dumps(response.json(), indent=4, separators=(',', ': ')))
 
 r_resp=response.json()
 
-print(r_resp["response"][0]["hostIp"]["id"]["hostMac"])
+print(r_resp["response"][0]["hostIp"])
+print(r_resp["response"][0]["hostMac"])
+print(r_resp["response"][0]["id"])
 
